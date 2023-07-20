f22195632af89c6b466f811f4e6d47b6f275d8cb
Seiya Miyazaki <68964442+samiyazaki@users.noreply.github.com>
2023-07-20 14:13:27 -0400

    Adjusting python project

--- practice.py
+++ practice.py
@@ -1,1 +1,22 @@
-print("Hello World")
+import random
+
+def greeting_generator():
+    greetings = [
+        "Hello, {}! It's great to see you.",
+        "Hi, {}! Hope you're having a good day.",
+        "Hey, {}! What's up?",
+        "Good to see you, {}! How have you been?",
+        "Hello, {}! Nice to meet you."
+    ]
+
+    while True:
+        name = input("\nPlease enter your name (or type 'exit' to stop): ")
+        
+        if name.lower() == 'exit':
+            print("Goodbye!")
+            break
+
+        random_greeting = random.choice(greetings)
+        print(random_greeting.format(name))
+
+greeting_generator()
